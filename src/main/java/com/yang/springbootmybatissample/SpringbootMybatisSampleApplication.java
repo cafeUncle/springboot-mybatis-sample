5077004504ef3a198bcffd94b2100dbe442878d1
yangy_coder@163.com <yangy_coder@163.com>
2018-07-22 15:28:30 +0800

    init

---
+++ src/main/java/com/yang/springbootmybatissample/SpringbootMybatisSampleApplication.java
@@ -0,0 +1,12 @@
+package com.yang.springbootmybatissample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisSampleApplication.class, args);
+	}
+}
